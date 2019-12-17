68d4a4f591513c240f439be2d2f1d66dfae76d3c
dongyul.Im <nontile@daum.net>
2019-12-17 09:50:00 +0900

    site register

---
+++ mysite2/polls/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from polls.models import Question, Choice
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+def index(request):
+    log.error(__name__)
+    lastest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'lastest_question_list': lastest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You don't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html/', {'question': question})
