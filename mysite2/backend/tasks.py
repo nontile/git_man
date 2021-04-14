e5bb8fb78d29e1dac494d13a53841b0d672715a7
nontile <nontile@users.noreply.github.com>
2021-04-14 14:11:56 +0900

    Delete mysite2 directory

--- mysite2/backend/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-
-app = Celery('test', broker='amqp://sktl:sktl01@192.168.200.118:5672/sktl')
-# app = Celery('test', broker='amqp://alis:alis1234!@#$@192.168.200.118:5672/myvhost')
-
-
-@app.task
-def add(x, y):
-    return x + y
